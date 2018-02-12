a95a5a905f0db45127c83555de4196d534a5d62b
alfonso <alphaandsew@gmail.com>
2018-02-12 08:13:14 -0800

    add placeholder 'subdirs'

--- pathfile.py
+++ pathfile.py
@@ -12,9 +12,10 @@ image_list = open("output.txt","w+")
 for path, subdirs, files in os.walk("."):
     for x in files:
         if x.endswith(".jpg"):
-            image_list.write(os.path.join(path, x) +"\n")
+            image_list.write(os.path.join(path, x) + ", " + str(subdirs) +"\n")
         if x.endswith(".png"):
-            image_list.write(os.path.join(path, x) + "\n")
+            image_list.write(os.path.join(path, x) + ", " + str(subdirs) + "\n")
+
 image_list.close()
 image_list = open("output.txt","r")
 print (image_list.read())
