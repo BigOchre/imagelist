864d495871d340c2c92d044c7fa62aa0bde47964
alfonso <alphaandsew@gmail.com>
2018-02-12 08:22:33 -0800

    add path to line, move on to formatting

--- pathfile.py
+++ pathfile.py
@@ -12,10 +12,11 @@ image_list = open("output.txt","w+")
 for path, subdirs, files in os.walk("."):
     for x in files:
         if x.endswith(".jpg"):
-            image_list.write(os.path.join(path, x) + ", " + str(subdirs) +"\n")
+            image_list.write(os.path.join(path, x) + ", " + str(path) +"\n") #testing path
         if x.endswith(".png"):
-            image_list.write(os.path.join(path, x) + ", " + str(subdirs) + "\n")
+            image_list.write(os.path.join(path, x) + ", " + str(path) +"\n") #testing path
 
 image_list.close()
-image_list = open("output.txt","r")
-print (image_list.read())
+
+with open("output.txt","r") as print_list: #so I can look at it in console
+	print (print_list.read())
